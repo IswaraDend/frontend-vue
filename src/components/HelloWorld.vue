23e37ab0a6a20a51f9cfe9637ce5c91187cd4212
Iswara Dendy A <126663320+IswaraDend@users.noreply.github.com>
2023-11-01 17:57:16 +0700

    Frontend vue.js

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>Welcome to home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+
---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="row">
+        <div class="col-sm-4">
+            <h2 align="center"> Login </h2>
+
+            <form action="form-group" align="left"></form>
+        </div>
+    </div>
+
+</template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+  </script>
+  
